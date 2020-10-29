e2d7ecd3acc2a2fde47d699725b08ee6178d42c9
lun <caizhuolun@ruqimobility.com>
2020-10-29 17:17:05 +0800

    python 脚本编写实战（二）- 作业 (3)天山童姥类虚竹类

--- test_python/test_python_03.py
+++ test_python/test_python_03.py
@@ -14,4 +14,30 @@ class TongLao:
         else:
             raise ValueError("传入名字错误")
 
+    def fight_zms(self, enemy_hp, enemy_power):
+        self.hp = self.hp / 2
+        self.power = self.power * 10
+        self.hp = self.hp - enemy_power
+        enemy_hp = enemy_hp - self.power
+        if self.hp > enemy_hp:
+            print(f"我的血量是{self.hp},敌人的血量是{enemy_hp}")
+            print("I win")
+        elif self.hp < enemy_hp:
+            print(f"我的血量是{self.hp},敌人的血量是{enemy_hp}")
+            print("Enemy win")
+        else:
+            print(f"我的血量是{self.hp},敌人的血量是{enemy_hp}")
+            print("两败俱伤")
 
+class XuZhu(TongLao):
+    def __init__(self, hp, power):
+        super().__init__(hp, power)
+
+    def read(self):
+        print("罪过罪过")
+
+T = TongLao(2000,10)
+T.see_people("WYZ")
+T.fight_zms(1000,200)
+X = XuZhu(2000,10)
+X.read()
